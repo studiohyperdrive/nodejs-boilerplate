08f8acf0e3b6d32fd2a41b8155c5e161c4ebd8e1
Niels Bril <niels.bril@hotmail.com>
2019-06-18 17:43:30 +0200

    feat: add primitive application with status route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,23 @@
-import { default as express, Application } from 'express';
+import { default as express, Application, Request, Response, NextFunction } from 'express';
 import { Server } from 'http';
 import { AddressInfo } from 'net';
+
+import { version } from '../package.json';
 
 const app: Application = express();
 let server: Server;
 
-server = app.listen(process.env.PORT, () => {
-	console.log(`Server running on ${process.env.NODE_ENV} environment at port ${(server.address() as AddressInfo).port}`);
+app.route('/status').get((req: Request, res:Response, next: NextFunction) => {
+	return res.status(200).json({
+		version,
+		success: true,
+	});
 });
+
+if (process.env.NODE_ENV !== 'test') {
+	server = app.listen(process.env.PORT, () => {
+		console.log(`Server running on ${process.env.NODE_ENV} environment at port ${(server.address() as AddressInfo).port}`); // tslint:disable-line no-console
+	});
+}
+
+export default app;
